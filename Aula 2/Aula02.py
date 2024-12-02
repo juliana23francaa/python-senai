f715eadf267e2a35a142fa222b20395e7c904e97
Juliana França <julianafranca.work@gmail.com>
2024-12-02 10:00:21 -0300

    aprendi oq é uma variável

---
+++ Aula 2/Aula02.py
@@ -0,0 +1,8 @@
+print('bom dia a todos')
+
+nome_usuario =  'Juliana Brito Franca' # tipo -> str = string
+idade_usuario = 19 # tipo -> int = integer
+altura_usuario = 1.68 # tipo -> float
+maior_de_idade = True # tipo -> bool = boolean
+
+print('Seja bem vindo', nome_usuario , 'sua idade é', idade_usuario, 'sua altura é', altura_usuario, 'você é maior de idade?', maior_de_idade)
